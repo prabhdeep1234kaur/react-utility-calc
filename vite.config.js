de209712f8c6601c25c508704c465f4fee184c59
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-22 20:40:42 +0530

    github-pages-vite-dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/your-repo-name/' : '/', // Use the repo name in production and root in development
+  base: command === 'build' ? '/react-utility-calc/' : '/', // Use the repo name in production and root in development
 }));
