975a28024c90cb9c7a894bc3528eae72913048d6
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-22 20:39:21 +0530

    github-pages-deploy dist and vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/prabhdeep1234kaur.github.io/',  // Replace with your actual GitHub repository name
-})
+  base: command === 'build' ? '/your-repo-name/' : '/', // Use the repo name in production and root in development
+}));
