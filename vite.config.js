21bbc77163d9ee2b0d182e15c430b0913ea83497
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-22 20:29:12 +0530

    github-pages-vite-base-name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-utility-calc/',  // Replace with your actual GitHub repository name
+  base: '/prabhdeep1234kaur.github.io/',  // Replace with your actual GitHub repository name
 })
