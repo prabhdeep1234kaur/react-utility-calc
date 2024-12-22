2f58ae1e39782b2a0912d6a8a09ed60637f263e2
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-22 20:25:21 +0530

    guthub-pages-vite-changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensure this is set to 'dist'
-  }
+  base: '/react-utility-calc/',  // Replace with your actual GitHub repository name
 })
