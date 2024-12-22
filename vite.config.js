8213c3c218504f204361a51adee941dae1b3bfc6
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-22 20:07:56 +0530

    github-vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensure this is set to 'dist'
-  }
+  
 })
