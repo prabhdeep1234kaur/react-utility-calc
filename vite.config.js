b9ce4434dfab91c4ef62c7ec2e08d16ebcfa3880
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-12-23 19:00:24 +0530

    github-pages-config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-utility-calc/',
   plugins: [react()],
   build: {
     outDir: 'dist' // Ensure this is set to 'dist'
